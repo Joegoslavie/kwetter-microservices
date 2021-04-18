38584c54d6f2e489687785370e53253da181177e
Vaessen,Hugo H.H.G <hugo.vaessen@student.fontys.nl>
2021-04-18 13:53:51 +0200

    profile service operations

---
+++ ProfileServices/Kwetter.ProfileService/Business/ProfileManager.cs
@@ -0,0 +1,22 @@
+﻿namespace Kwetter.ProfileService.Business
+{
+    using Kwetter.ProfileService.Persistence.Context;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class ProfileManager
+    {
+        private readonly ProfileContext context;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProfileManager"/> class.
+        /// </summary>
+        /// <param name="context">Profile context class.</param>
+        public ProfileManager(ProfileContext context)
+        {
+            this.context = context;
+        }
+    }
+}
---
+++ ProfileServices/Kwetter.ProfileService/Services/ProfileService.cs
@@ -0,0 +1,61 @@
+﻿namespace Kwetter.ProfileService.Services
+{
+    using Grpc.Core;
+    using Kwetter.ProfileService.Business;
+    using Microsoft.Extensions.Logging;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ProfileService : ProfileGRPCService.ProfileGRPCServiceBase
+    {
+        private readonly ILogger<ProfileService> logger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProfileService"/> class.
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="manager"></param>
+        public ProfileService(ILogger<ProfileService> logger, ProfileManager manager)
+        {
+            this.logger = logger;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override Task<ProfileResponse> GetAll(ProfileRequest request, ServerCallContext context)
+        {
+            return base.GetAll(request, context);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override Task<ProfileResponse> GetProfile(ProfileRequest request, ServerCallContext context)
+        {
+            return base.GetProfile(request, context);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override Task<ProfileResponse> CreateProfile(UpdateProfileRequest request, ServerCallContext context)
+        {
+            return base.CreateProfile(request, context);
+        }
+    }
+}
