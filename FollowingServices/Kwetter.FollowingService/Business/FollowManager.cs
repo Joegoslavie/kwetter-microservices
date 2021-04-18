e85ecfa80713dcfdf084573405c34646a49fcd08
Vaessen,Hugo H.H.G <hugo.vaessen@student.fontys.nl>
2021-04-18 15:07:31 +0200

    follow stuff

---
+++ FollowingServices/Kwetter.FollowingService/Business/FollowManager.cs
@@ -0,0 +1,17 @@
+﻿using Kwetter.FollowingService.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kwetter.FollowingService.Business
+{
+    public class FollowManager
+    {
+        private readonly FollowingContext context;
+        public FollowManager(FollowingContext context)
+        {
+            this.context = context;
+        }
+    }
+}
---
+++ FollowingServices/Kwetter.FollowingService/Services/FollowingService.cs
@@ -0,0 +1,34 @@
+﻿using Grpc.Core;
+using Kwetter.FollowingService.Business;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kwetter.FollowingService.Services
+{
+    public class FollowingService : FollowGRPCService.FollowGRPCServiceBase
+    {
+        private readonly ILogger<FollowingService> logger;
+
+        private readonly FollowManager manager;
+
+        public FollowingService(ILogger<FollowingService> logger, FollowManager manager)
+        {
+            this.logger = logger;
+            this.manager = manager;
+        }
+
+        public override Task<OperationResponse> ToggleFollow(FollowRequest request, ServerCallContext context)
+        {
+            return base.ToggleFollow(request, context);
+        }
+
+        public override Task<OperationResponse> ToggleBlock(BlockRequest request, ServerCallContext context)
+        {
+            return base.ToggleBlock(request, context);
+        }
+
+    }
+}
