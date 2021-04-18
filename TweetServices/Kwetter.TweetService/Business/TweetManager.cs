b2ef6743c4edf9bec0da4d55933ec1b8df320599
Vaessen,Hugo H.H.G <hugo.vaessen@student.fontys.nl>
2021-04-18 14:25:10 +0200

    tweet related thingys

---
+++ TweetServices/Kwetter.TweetService/Business/TweetManager.cs
@@ -0,0 +1,17 @@
+﻿namespace Kwetter.TweetService.Business
+{
+    using Kwetter.TweetService.Persistence.Context;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class TweetManager
+    {
+        private readonly TweetContext context;
+        public TweetManager(TweetContext context)
+        {
+            this.context = context;
+        }
+    }
+}
---
+++ TweetServices/Kwetter.TweetService/Services/TweetService.cs
@@ -0,0 +1,58 @@
+﻿namespace Kwetter.TweetService.Services
+{
+    using Grpc.Core;
+    using Kwetter.TweetService.Business;
+    using Microsoft.Extensions.Logging;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class TweetService : TweetGRPCService.TweetGRPCServiceBase
+    {
+        /// <summary>
+        /// Default logging instance.
+        /// </summary>
+        private readonly ILogger<TweetService> logger;
+
+        /// <summary>
+        /// Manager class for tweets.
+        /// </summary>
+        private readonly TweetManager manager;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TweetService"/> class.
+        /// </summary>
+        /// <param name="logger">Injected logger.</param>
+        /// <param name="manager">Injected manager.</param>
+        public TweetService(ILogger<TweetService> logger, TweetManager manager)
+        {
+            this.logger = logger;
+            this.manager = manager;
+        }
+
+        public override Task<TweetResponse> GetByUser(RetrieveTweetRequest request, ServerCallContext context)
+        {
+            return base.GetByUser(request, context);
+        }
+
+        public override Task<TweetResponse> GetByHashtag(RetrieveTweetRequest request, ServerCallContext context)
+        {
+            return base.GetByHashtag(request, context);
+        }
+
+        public override Task<TweetResponse> PlaceTweet(TweetOperationRequest request, ServerCallContext context)
+        {
+            return base.PlaceTweet(request, context);
+        }
+
+        public override Task<TweetResponse> ToggleLike(TweetOperationRequest request, ServerCallContext context)
+        {
+            return base.ToggleLike(request, context);
+        }
+
+    }
+}
