a0e3953b66ef19c2e992546d872cbc1759156e7a
Joegoslavie <hugovaessen@gmail.com>
2021-05-20 18:51:56 +0200

    test

--- AuthenticationServices/Kwetter.AuthenticationService.Persistence/Context/AuthenticationContext.cs
+++ AuthenticationServices/Kwetter.AuthenticationService.Persistence/Context/AuthenticationContext.cs
@@ -1,14 +1,13 @@
-﻿using Kwetter.AuthenticationService.Persistence.Entity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Kwetter.AuthenticationService.Persistence.Context
+﻿namespace Kwetter.AuthenticationService.Persistence.Context
 {
+    using Kwetter.AuthenticationService.Persistence.Entity;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
     public class AuthenticationContext : IdentityDbContext<KwetterUserEntity<int>, KwetterRoleEntity<int>, int>
     {
         public AuthenticationContext(DbContextOptions<AuthenticationContext> options)
