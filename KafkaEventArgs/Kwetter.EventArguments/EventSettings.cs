948f43331656a1471fd7783aba5b6aaea29d759a
Hugo Vaessen <hugo.vaessen@student.fontys.nl>
2021-06-13 22:40:05 +0200

    add hashtag and mention topic properties

--- KafkaEventArgs/Kwetter.EventArguments/EventSettings.cs
+++ KafkaEventArgs/Kwetter.EventArguments/EventSettings.cs
@@ -9,17 +9,27 @@ namespace Kwetter.Messaging
     public static class EventSettings
     {
         /// <summary>
-        /// 
+        /// New profile event topic.
         /// </summary>
         public static string NewProfileEventTopic { get; } = "NewProfileEvent";
 
         /// <summary>
-        /// 
+        /// New tweet profile event topic.
         /// </summary>
-        public static string TweetProfileEventTopic { get; } = "TweetProfileEvent";
+        public static string NewTweetProfileEventTopic { get; } = "TweetProfileEvent";
 
         /// <summary>
-        /// 
+        /// New mention in tweet event topic.
+        /// </summary>
+        public static string NewTweetMentionEventTopic { get; } = "NewTweetMentionEvent";
+
+        /// <summary>
+        /// Hashtag in tweet event topic.
+        /// </summary>
+        public static string TweetContainsHashtagEventTopic { get; } = "TweetContainsHashtagEvent";
+
+        /// <summary>
+        /// Update tweet reference profile event topic.
         /// </summary>
         public static string TweetProfileUpdateEventTopic { get; } = "TweetProfileUpdateEvent";
     }
